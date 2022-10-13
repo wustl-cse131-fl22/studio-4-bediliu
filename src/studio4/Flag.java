f143d302936f64e02d8b922baac8e2db7dadb72d
13380 <13380@zy03.dhcp.wustl.edu>
2022-10-13 14:04:12 -0500

    done

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -6,6 +6,10 @@ import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
+		StdDraw.setPenColor(Color.BLACK);
+		StdDraw.filledSquare(0, 0, 1);
+		StdDraw.setPenColor(Color.RED);
+		StdDraw.filledCircle(0.5,0.5,0.2);
 		
 	}
 }
